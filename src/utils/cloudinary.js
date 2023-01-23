658cad5a950196385fd18667c560f4242a8a8b6e
Lucio Vasquez <castrocociolucio@gmail.com>
2023-01-23 17:01:33 -0300

    :sparkles: adding utils folder and cloudinary config

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,12 @@
+import cloudinary from "cloudinary"
+
+cloudinary.congif({
+
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
+    secure: true,
+
+})
+
+export default cloudinary;
