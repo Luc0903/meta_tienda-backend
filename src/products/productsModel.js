5e7559dc17bc91911f6e4d29151f361e311e8a66
Lucio Vasquez <castrocociolucio@gmail.com>
2023-01-24 09:19:41 -0300

    :sparkles: Adding the products model

---
+++ src/products/productsModel.js
@@ -0,0 +1,42 @@
+import mongoose, { model } from "mongoose";
+
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please provide a name'],
+        minLength: 3,
+        maxLength: 30,
+        trim: true,
+    },
+    image: {
+        public_id: {
+            type: String,
+            required: true,
+        },
+        url: {
+            type: String,
+            required: true,
+        }
+    },
+    description: {
+        type: String, 
+        minLength: 10,
+        maxLength: 100,
+        trim: true,
+        required: true,
+    },
+    price: {
+        type: Number, 
+        required: true,
+        default: 0,
+    },
+    stock: {
+        type: Number, 
+        required: true,
+        default: 0,
+    } 
+})
+
+const Product = model('Product', productSchema)
+
+export default Product;
