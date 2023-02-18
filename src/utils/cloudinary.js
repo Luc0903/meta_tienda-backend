48889116917cd87d94983f8ea15a61376c215e7a
Lucio Vasquez <castrocociolucio@gmail.com>
2023-02-18 02:15:08 -0300

    :zap: express-fileupload added

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,24 +1,25 @@
-import cloudinary from "cloudinary"
+import cloudinary from "cloudinary";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../../config.env" });
 
 cloudinary.congif({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+  secure: true,
+});
 
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET,
-    secure: true,
+export const uploadImgCloduinary = async (filePath) => {
+  return await cloudinary.v2.uploader.upload(filePath, {
+    folder: "metatienda",
+  });
+};
 
-})
-
-export const uploadImgCloduinary = async filePath => {
-    return await cloudinary.v2.uploader.upload(filePath, {
-        folder: 'metatienda'
-    })
-}
-
-export const deleteImgCloudinary = async filePath => {
-    return await cloudinary.v2.uploader.destroy(filePath, {
-        folder: 'metatienda'
-    })
-}
+export const deleteImgCloudinary = async (filePath) => {
+  return await cloudinary.v2.uploader.destroy(filePath, {
+    folder: "metatienda",
+  });
+};
 
 export default cloudinary;
