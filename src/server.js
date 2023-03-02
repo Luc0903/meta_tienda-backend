cad3b5a9dab85b7399f5e7d926d0f70f3c33fa95
Lucio Vasquez <castrocociolucio@gmail.com>
2023-03-02 17:40:03 -0300

    :recycle: Moved the env file to src and changed te path object inside dotenv config

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import app from "./app.js";
 
-dotenv.config({ path: "../.env" });
+dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,25 +1,28 @@
 import cloudinary from "cloudinary";
 import dotenv from "dotenv";
 
-dotenv.config({ path: "../../.env" });
+dotenv.config();
 
+// `${process.env.CLOUDINARY_URL}`
 cloudinary.config({
-  cloud_name: `${process.env.CLOUD_NAME}`,
-  api_key: Number(process.env.API_KEY),
-  api_secret: `${process.env.API_SECRET}`,
-  secure: true,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+  // cloud_name: "dctqxutsq",
+  // api_key: 427528858229588,
+  // api_secret: "83vwSDPIdkPoJMbAY4lGjJFHBZ4",
 });
 
 export const uploadImgCloduinary = async (filePath) => {
-  return await cloudinary.v2.uploader.upload(filePath, {
+  return await cloudinary.uploader.upload(filePath, {
     folder: "metatienda",
   });
 };
 
-export const deleteImgCloudinary = async (filePath) => {
-  return await cloudinary.v2.uploader.destroy(filePath, {
-    folder: "metatienda",
-  });
-};
+// export const deleteImgCloudinary = async (filePath) => {
+//   return await cloudinary.v2.uploader.destroy(filePath, {
+//     folder: "metatienda",
+//   });
+// };
 
 export default cloudinary;
