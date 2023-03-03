9e04ea76d5c7994e427eb0f76f979b760927da00
Lucio Vasquez <castrocociolucio@gmail.com>
2023-03-03 13:34:40 -0300

    :art: Deleting comments and formatting code

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -3,26 +3,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// `${process.env.CLOUDINARY_URL}`
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
-  // cloud_name: "dctqxutsq",
-  // api_key: 427528858229588,
-  // api_secret: "83vwSDPIdkPoJMbAY4lGjJFHBZ4",
 });
 
 export const uploadImgCloduinary = async (filePath) => {
-  return await cloudinary.uploader.upload(filePath, {
-    folder: "metatienda",
-  });
+  return await cloudinary.uploader.upload(filePath, { folder: "metatienda" });
 };
-
-// export const deleteImgCloudinary = async (filePath) => {
-//   return await cloudinary.v2.uploader.destroy(filePath, {
-//     folder: "metatienda",
-//   });
-// };
 
 export default cloudinary;
